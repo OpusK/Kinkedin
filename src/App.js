ca35909c3d6b3d11c396efeca6783e91cb59aca8
Kei <opusk44@gmail.com>
2022-02-22 23:02:19 +0900

    add logo & search bar UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Ready for making!</h1>
-
       {/* Header */}
-      
+      <Header />
+
       {/* App Body */}
         {/* Sidebar */}
         {/* Feed */}
