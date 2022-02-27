362d56b9640f8c66960e3854fc129ad7b34e4553
Kei <opusk44@gmail.com>
2022-02-27 22:50:37 +0900

    add nav bar on header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderNavBar from './HeaderNavBar.js'
+
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+
 
 function Header() {
   return (
@@ -10,12 +19,17 @@ function Header() {
 
         <div className="header__search">
           <SearchIcon />
-          <input type="text"/>
+          <input type="text" />
         </div>
       </div>
 
       <div className="header__right">
-
+        <HeaderNavBar Icon={HomeIcon} title='Home' />
+        <HeaderNavBar Icon={SupervisorAccountIcon} title='My Network' />
+        <HeaderNavBar Icon={BusinessCenterIcon} title='Jobs' />
+        <HeaderNavBar Icon={ChatIcon} title='Messaging' />
+        <HeaderNavBar Icon={NotificationsIcon} title='Notification' />
+        <HeaderNavBar avatar="https://avatars.githubusercontent.com/u/18085958?s=400&u=5ed6e2c60cb0e4fc413b801abf470ef40fde02fe&v=4" title='me' />
       </div>
     </div>
   );
