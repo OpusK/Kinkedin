6dff19f00f5847008ee05e25d69bf72addfdcac7
Kei <opusk44@gmail.com>
2022-03-02 21:39:53 +0900

    import redux selector & userSlice to create Login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from "./features/userSlice";
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
